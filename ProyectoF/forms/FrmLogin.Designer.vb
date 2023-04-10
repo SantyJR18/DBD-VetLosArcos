3d68d90a775dc26bd0a179ea0d16a188fd8e67fd
SantyJR18 <manzanares.santiago@hotmail.com>
2023-04-10 00:19:41 -0600

    Login

---
+++ ProyectoF/forms/FrmLogin.Designer.vb
@@ -0,0 +1,189 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmLogin))
+        Me.TitleBar = New System.Windows.Forms.Panel()
+        Me.BtnMinimize = New System.Windows.Forms.PictureBox()
+        Me.BtnClose = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.LblUsuario = New System.Windows.Forms.Label()
+        Me.TxtUsuario = New System.Windows.Forms.TextBox()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.LblPassword = New System.Windows.Forms.Label()
+        Me.BtnLogin = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TitleBar.SuspendLayout()
+        CType(Me.BtnMinimize, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BtnClose, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TitleBar
+        '
+        Me.TitleBar.BackColor = System.Drawing.Color.DodgerBlue
+        Me.TitleBar.Controls.Add(Me.BtnMinimize)
+        Me.TitleBar.Controls.Add(Me.BtnClose)
+        Me.TitleBar.Dock = System.Windows.Forms.DockStyle.Top
+        Me.TitleBar.Location = New System.Drawing.Point(0, 0)
+        Me.TitleBar.Name = "TitleBar"
+        Me.TitleBar.Size = New System.Drawing.Size(427, 50)
+        Me.TitleBar.TabIndex = 0
+        '
+        'BtnMinimize
+        '
+        Me.BtnMinimize.Image = Global.ProyectoF.My.Resources.Resources.minusIcon
+        Me.BtnMinimize.Location = New System.Drawing.Point(357, 12)
+        Me.BtnMinimize.Name = "BtnMinimize"
+        Me.BtnMinimize.Size = New System.Drawing.Size(21, 26)
+        Me.BtnMinimize.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.BtnMinimize.TabIndex = 3
+        Me.BtnMinimize.TabStop = False
+        '
+        'BtnClose
+        '
+        Me.BtnClose.Image = Global.ProyectoF.My.Resources.Resources.closeIcon
+        Me.BtnClose.Location = New System.Drawing.Point(384, 12)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(25, 25)
+        Me.BtnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.BtnClose.TabIndex = 2
+        Me.BtnClose.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Panel2.Location = New System.Drawing.Point(0, 641)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(427, 15)
+        Me.Panel2.TabIndex = 1
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = Global.ProyectoF.My.Resources.Resources.HuellaAnimal
+        Me.PictureBox3.Location = New System.Drawing.Point(115, 117)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(194, 174)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'LblUsuario
+        '
+        Me.LblUsuario.AutoSize = True
+        Me.LblUsuario.Font = New System.Drawing.Font("High Tower Text", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblUsuario.Location = New System.Drawing.Point(65, 333)
+        Me.LblUsuario.Name = "LblUsuario"
+        Me.LblUsuario.Size = New System.Drawing.Size(84, 23)
+        Me.LblUsuario.TabIndex = 5
+        Me.LblUsuario.Text = "Usuario"
+        '
+        'TxtUsuario
+        '
+        Me.TxtUsuario.Location = New System.Drawing.Point(69, 370)
+        Me.TxtUsuario.Name = "TxtUsuario"
+        Me.TxtUsuario.Size = New System.Drawing.Size(277, 22)
+        Me.TxtUsuario.TabIndex = 6
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Location = New System.Drawing.Point(69, 463)
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.Size = New System.Drawing.Size(277, 22)
+        Me.TxtPassword.TabIndex = 8
+        '
+        'LblPassword
+        '
+        Me.LblPassword.AutoSize = True
+        Me.LblPassword.Font = New System.Drawing.Font("High Tower Text", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblPassword.Location = New System.Drawing.Point(65, 424)
+        Me.LblPassword.Name = "LblPassword"
+        Me.LblPassword.Size = New System.Drawing.Size(114, 23)
+        Me.LblPassword.TabIndex = 7
+        Me.LblPassword.Text = "Contraseña"
+        '
+        'BtnLogin
+        '
+        Me.BtnLogin.BackColor = System.Drawing.Color.DodgerBlue
+        Me.BtnLogin.Font = New System.Drawing.Font("High Tower Text", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnLogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.BtnLogin.Location = New System.Drawing.Point(115, 517)
+        Me.BtnLogin.Name = "BtnLogin"
+        Me.BtnLogin.Size = New System.Drawing.Size(177, 52)
+        Me.BtnLogin.TabIndex = 9
+        Me.BtnLogin.Text = "Entrar "
+        Me.BtnLogin.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("High Tower Text", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(89, 74)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(248, 28)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Veterinaria Los Arcos"
+        '
+        'FrmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(427, 656)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnLogin)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.LblPassword)
+        Me.Controls.Add(Me.TxtUsuario)
+        Me.Controls.Add(Me.LblUsuario)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.TitleBar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "FrmLogin"
+        Me.Text = "Login"
+        Me.TitleBar.ResumeLayout(False)
+        CType(Me.BtnMinimize, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BtnClose, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TitleBar As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents BtnClose As PictureBox
+    Friend WithEvents BtnMinimize As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents LblUsuario As Label
+    Friend WithEvents TxtUsuario As TextBox
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents LblPassword As Label
+    Friend WithEvents BtnLogin As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ ProyectoF/forms/FrmLogin.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Runtime.InteropServices
+Public Class FrmLogin
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        Application.Exit()
+    End Sub
+    Private Sub BtnMinimize_Click(sender As Object, e As EventArgs) Handles BtnMinimize.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub CustomizeComponents()
+        TxtPassword.UseSystemPasswordChar = True
+    End Sub
+
+    Public Sub New()
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        CustomizeComponents()
+    End Sub
+
+#Region "Arrastrar/Drag Form"
+
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(hWnd As IntPtr, wMsg As Integer, wParam As Integer, lParam As Integer)
+    End Sub
+    Private Sub titleBar_MouseDown(sender As Object, e As MouseEventArgs) Handles TitleBar.MouseDown
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+
+#End Region
+
+End Class
