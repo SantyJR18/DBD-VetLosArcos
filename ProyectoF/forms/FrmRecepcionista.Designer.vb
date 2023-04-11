8b0639a963a009b054153f3c8d82c54aee354948
Camilo Solís <cami.javiers12@gmail.com>
2023-04-10 22:18:43 -0600

    Agregue los formularios Recepcionista y RegistrarCliente. Tambien elimine la carpeta iconos y agregue nuevas imagenes

---
+++ ProyectoF/forms/FrmRecepcionista.Designer.vb
@@ -0,0 +1,344 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmRecepcionista
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmRecepcionista))
+        Me.PnlTitle = New System.Windows.Forms.Panel()
+        Me.BtnRestaurar = New System.Windows.Forms.PictureBox()
+        Me.BtnMaximizar = New System.Windows.Forms.PictureBox()
+        Me.BtnMinimizar = New System.Windows.Forms.PictureBox()
+        Me.BtnCerrar = New System.Windows.Forms.PictureBox()
+        Me.PnlContainer = New System.Windows.Forms.Panel()
+        Me.Panel6 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.BtnLogOut = New System.Windows.Forms.Button()
+        Me.BtnRegPac = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.BtnEditarReg = New System.Windows.Forms.Button()
+        Me.BtnRegClient = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PnlContent = New System.Windows.Forms.Panel()
+        Me.PnlTitle.SuspendLayout()
+        CType(Me.BtnRestaurar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BtnMaximizar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BtnMinimizar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.BtnCerrar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.PnlContainer.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PnlTitle
+        '
+        Me.PnlTitle.BackColor = System.Drawing.Color.DodgerBlue
+        Me.PnlTitle.Controls.Add(Me.BtnRestaurar)
+        Me.PnlTitle.Controls.Add(Me.BtnMaximizar)
+        Me.PnlTitle.Controls.Add(Me.BtnMinimizar)
+        Me.PnlTitle.Controls.Add(Me.BtnCerrar)
+        Me.PnlTitle.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PnlTitle.Location = New System.Drawing.Point(0, 0)
+        Me.PnlTitle.Name = "PnlTitle"
+        Me.PnlTitle.Size = New System.Drawing.Size(826, 38)
+        Me.PnlTitle.TabIndex = 0
+        '
+        'BtnRestaurar
+        '
+        Me.BtnRestaurar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnRestaurar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnRestaurar.Image = Global.ProyectoF.My.Resources.Resources.miniIcon
+        Me.BtnRestaurar.Location = New System.Drawing.Point(740, 6)
+        Me.BtnRestaurar.Name = "BtnRestaurar"
+        Me.BtnRestaurar.Size = New System.Drawing.Size(25, 25)
+        Me.BtnRestaurar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.BtnRestaurar.TabIndex = 1
+        Me.BtnRestaurar.TabStop = False
+        Me.BtnRestaurar.Visible = False
+        '
+        'BtnMaximizar
+        '
+        Me.BtnMaximizar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnMaximizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnMaximizar.Image = Global.ProyectoF.My.Resources.Resources.maxIcon
+        Me.BtnMaximizar.Location = New System.Drawing.Point(740, 6)
+        Me.BtnMaximizar.Name = "BtnMaximizar"
+        Me.BtnMaximizar.Size = New System.Drawing.Size(25, 25)
+        Me.BtnMaximizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.BtnMaximizar.TabIndex = 3
+        Me.BtnMaximizar.TabStop = False
+        '
+        'BtnMinimizar
+        '
+        Me.BtnMinimizar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnMinimizar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnMinimizar.Image = Global.ProyectoF.My.Resources.Resources.minusIcon
+        Me.BtnMinimizar.Location = New System.Drawing.Point(699, 6)
+        Me.BtnMinimizar.Name = "BtnMinimizar"
+        Me.BtnMinimizar.Size = New System.Drawing.Size(25, 25)
+        Me.BtnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.BtnMinimizar.TabIndex = 2
+        Me.BtnMinimizar.TabStop = False
+        '
+        'BtnCerrar
+        '
+        Me.BtnCerrar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnCerrar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnCerrar.Image = Global.ProyectoF.My.Resources.Resources.reject
+        Me.BtnCerrar.Location = New System.Drawing.Point(782, 6)
+        Me.BtnCerrar.Name = "BtnCerrar"
+        Me.BtnCerrar.Size = New System.Drawing.Size(25, 25)
+        Me.BtnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.BtnCerrar.TabIndex = 0
+        Me.BtnCerrar.TabStop = False
+        '
+        'PnlContainer
+        '
+        Me.PnlContainer.BackColor = System.Drawing.Color.LightSlateGray
+        Me.PnlContainer.Controls.Add(Me.Panel6)
+        Me.PnlContainer.Controls.Add(Me.Panel5)
+        Me.PnlContainer.Controls.Add(Me.Panel4)
+        Me.PnlContainer.Controls.Add(Me.Panel3)
+        Me.PnlContainer.Controls.Add(Me.Panel2)
+        Me.PnlContainer.Controls.Add(Me.Panel1)
+        Me.PnlContainer.Controls.Add(Me.Button4)
+        Me.PnlContainer.Controls.Add(Me.BtnLogOut)
+        Me.PnlContainer.Controls.Add(Me.BtnRegPac)
+        Me.PnlContainer.Controls.Add(Me.Button2)
+        Me.PnlContainer.Controls.Add(Me.BtnEditarReg)
+        Me.PnlContainer.Controls.Add(Me.BtnRegClient)
+        Me.PnlContainer.Controls.Add(Me.PictureBox1)
+        Me.PnlContainer.Dock = System.Windows.Forms.DockStyle.Left
+        Me.PnlContainer.Location = New System.Drawing.Point(0, 38)
+        Me.PnlContainer.Name = "PnlContainer"
+        Me.PnlContainer.Size = New System.Drawing.Size(220, 509)
+        Me.PnlContainer.TabIndex = 0
+        '
+        'Panel6
+        '
+        Me.Panel6.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel6.Location = New System.Drawing.Point(0, 302)
+        Me.Panel6.Name = "Panel6"
+        Me.Panel6.Size = New System.Drawing.Size(5, 32)
+        Me.Panel6.TabIndex = 1
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel5.Location = New System.Drawing.Point(0, 264)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(5, 32)
+        Me.Panel5.TabIndex = 1
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel4.Location = New System.Drawing.Point(0, 226)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(5, 32)
+        Me.Panel4.TabIndex = 1
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel3.Location = New System.Drawing.Point(0, 188)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(5, 32)
+        Me.Panel3.TabIndex = 1
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel2.Location = New System.Drawing.Point(0, 150)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(5, 32)
+        Me.Panel2.TabIndex = 7
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel1.Location = New System.Drawing.Point(0, 112)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(5, 32)
+        Me.Panel1.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.FlatAppearance.BorderSize = 0
+        Me.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("High Tower Text", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.Image = CType(resources.GetObject("Button4.Image"), System.Drawing.Image)
+        Me.Button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button4.Location = New System.Drawing.Point(3, 264)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(216, 32)
+        Me.Button4.TabIndex = 5
+        Me.Button4.Text = "Generar Factura"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'BtnLogOut
+        '
+        Me.BtnLogOut.FlatAppearance.BorderSize = 0
+        Me.BtnLogOut.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue
+        Me.BtnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnLogOut.Font = New System.Drawing.Font("High Tower Text", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnLogOut.ForeColor = System.Drawing.Color.White
+        Me.BtnLogOut.Image = CType(resources.GetObject("BtnLogOut.Image"), System.Drawing.Image)
+        Me.BtnLogOut.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.BtnLogOut.Location = New System.Drawing.Point(4, 302)
+        Me.BtnLogOut.Name = "BtnLogOut"
+        Me.BtnLogOut.Size = New System.Drawing.Size(216, 32)
+        Me.BtnLogOut.TabIndex = 6
+        Me.BtnLogOut.Text = "Cerrar Sesión"
+        Me.BtnLogOut.UseVisualStyleBackColor = True
+        '
+        'BtnRegPac
+        '
+        Me.BtnRegPac.FlatAppearance.BorderSize = 0
+        Me.BtnRegPac.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue
+        Me.BtnRegPac.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnRegPac.Font = New System.Drawing.Font("High Tower Text", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnRegPac.ForeColor = System.Drawing.Color.White
+        Me.BtnRegPac.Image = Global.ProyectoF.My.Resources.Resources.paws
+        Me.BtnRegPac.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.BtnRegPac.Location = New System.Drawing.Point(4, 150)
+        Me.BtnRegPac.Name = "BtnRegPac"
+        Me.BtnRegPac.Size = New System.Drawing.Size(216, 32)
+        Me.BtnRegPac.TabIndex = 2
+        Me.BtnRegPac.Text = "Registrar Paciente"
+        Me.BtnRegPac.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.FlatAppearance.BorderSize = 0
+        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("High Tower Text", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Image = CType(resources.GetObject("Button2.Image"), System.Drawing.Image)
+        Me.Button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.Button2.Location = New System.Drawing.Point(4, 226)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(216, 32)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Eliminar"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'BtnEditarReg
+        '
+        Me.BtnEditarReg.FlatAppearance.BorderSize = 0
+        Me.BtnEditarReg.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue
+        Me.BtnEditarReg.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnEditarReg.Font = New System.Drawing.Font("High Tower Text", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnEditarReg.ForeColor = System.Drawing.Color.White
+        Me.BtnEditarReg.Image = CType(resources.GetObject("BtnEditarReg.Image"), System.Drawing.Image)
+        Me.BtnEditarReg.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.BtnEditarReg.Location = New System.Drawing.Point(4, 188)
+        Me.BtnEditarReg.Name = "BtnEditarReg"
+        Me.BtnEditarReg.Size = New System.Drawing.Size(216, 32)
+        Me.BtnEditarReg.TabIndex = 3
+        Me.BtnEditarReg.Text = "Editar Registros"
+        Me.BtnEditarReg.UseVisualStyleBackColor = True
+        '
+        'BtnRegClient
+        '
+        Me.BtnRegClient.FlatAppearance.BorderSize = 0
+        Me.BtnRegClient.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DodgerBlue
+        Me.BtnRegClient.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnRegClient.Font = New System.Drawing.Font("High Tower Text", 12.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnRegClient.ForeColor = System.Drawing.Color.White
+        Me.BtnRegClient.Image = Global.ProyectoF.My.Resources.Resources.user
+        Me.BtnRegClient.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.BtnRegClient.Location = New System.Drawing.Point(4, 112)
+        Me.BtnRegClient.Name = "BtnRegClient"
+        Me.BtnRegClient.Size = New System.Drawing.Size(216, 32)
+        Me.BtnRegClient.TabIndex = 1
+        Me.BtnRegClient.Text = "Registrar Cliente"
+        Me.BtnRegClient.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.ProyectoF.My.Resources.Resources.logoVet
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(220, 83)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'PnlContent
+        '
+        Me.PnlContent.BackColor = System.Drawing.Color.Gainsboro
+        Me.PnlContent.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.PnlContent.Location = New System.Drawing.Point(220, 38)
+        Me.PnlContent.Name = "PnlContent"
+        Me.PnlContent.Size = New System.Drawing.Size(606, 509)
+        Me.PnlContent.TabIndex = 1
+        '
+        'FrmRecepcionista
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(826, 547)
+        Me.Controls.Add(Me.PnlContent)
+        Me.Controls.Add(Me.PnlContainer)
+        Me.Controls.Add(Me.PnlTitle)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "FrmRecepcionista"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.PnlTitle.ResumeLayout(False)
+        CType(Me.BtnRestaurar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BtnMaximizar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BtnMinimizar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.BtnCerrar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.PnlContainer.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PnlTitle As Panel
+    Friend WithEvents PnlContainer As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PnlContent As Panel
+    Friend WithEvents BtnCerrar As PictureBox
+    Friend WithEvents BtnRestaurar As PictureBox
+    Friend WithEvents BtnMaximizar As PictureBox
+    Friend WithEvents BtnMinimizar As PictureBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents BtnRegPac As Button
+    Friend WithEvents Button2 As Button
+    Public WithEvents BtnEditarReg As Button
+    Friend WithEvents BtnRegClient As Button
+    Friend WithEvents BtnLogOut As Button
+    Friend WithEvents Panel6 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel1 As Panel
+End Class
---
+++ ProyectoF/forms/FrmRegistrarCliente.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Runtime.InteropServices
+
+Public Class FrmRegistrarCliente
+    Private Sub BtnCerrar_Click(sender As Object, e As EventArgs) Handles BtnCerrar.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub BtnMaximizar_Click(sender As Object, e As EventArgs) Handles BtnMaximizar.Click
+        WindowState = FormWindowState.Maximized
+        BtnMaximizar.Visible = False
+        BtnRestaurar.Visible = True
+    End Sub
+
+    Private Sub BtnRestaurar_Click(sender As Object, e As EventArgs) Handles BtnRestaurar.Click
+        WindowState = FormWindowState.Normal
+        BtnRestaurar.Visible = False
+        BtnMaximizar.Visible = True
+    End Sub
+
+    Private Sub BtnMinimizar_Click(sender As Object, e As EventArgs) Handles BtnMinimizar.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
+    Private Sub BtnCerrar_MouseHover(sender As Object, e As EventArgs) Handles BtnCerrar.MouseHover
+        BtnCerrar.BackColor = Color.Red
+    End Sub
+
+    Private Sub BtnCerrar_MouseLeave(sender As Object, e As EventArgs) Handles BtnCerrar.MouseLeave
+        BtnCerrar.BackColor = Color.DodgerBlue
+    End Sub
+
+    Private Sub BtnMaximizar_MouseHover(sender As Object, e As EventArgs) Handles BtnMaximizar.MouseHover
+        BtnMaximizar.BackColor = Color.Red
+    End Sub
+
+    Private Sub BtnMaximizar_MouseLeave(sender As Object, e As EventArgs) Handles BtnMaximizar.MouseLeave
+        BtnMaximizar.BackColor = Color.DodgerBlue
+    End Sub
+
+    Private Sub BtnRestaurar_MouseHover(sender As Object, e As EventArgs) Handles BtnRestaurar.MouseHover
+        BtnRestaurar.BackColor = Color.Red
+    End Sub
+
+    Private Sub BtnRestaurar_MouseLeave(sender As Object, e As EventArgs) Handles BtnRestaurar.MouseLeave
+        BtnRestaurar.BackColor = Color.DodgerBlue
+    End Sub
+
+    Private Sub BtnMinimizar_MouseHover(sender As Object, e As EventArgs) Handles BtnMinimizar.MouseHover
+        BtnMinimizar.BackColor = Color.Red
+    End Sub
+
+    Private Sub BtnMinimizar_MouseLeave(sender As Object, e As EventArgs) Handles BtnMinimizar.MouseLeave
+        BtnMinimizar.BackColor = Color.DodgerBlue
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="ReleaseCapture")>
+    Private Shared Sub ReleaseCapture()
+    End Sub
+    <DllImport("user32.DLL", EntryPoint:="SendMessage")>
+    Private Shared Sub SendMessage(hWnd As IntPtr, wMsg As Integer, wParam As Integer, lParam As Integer)
+    End Sub
+    Private Sub titleBarRec_MouseDown(sender As Object, e As MouseEventArgs) Handles PnlTitle.MouseDown
+        ReleaseCapture()
+        SendMessage(Me.Handle, &H112&, &HF012&, 0)
+    End Sub
+End Class
