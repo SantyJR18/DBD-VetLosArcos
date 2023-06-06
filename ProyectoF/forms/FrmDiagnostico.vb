c9fc8466e4553575d4eb7e380e12a5f4c9e0546f
SantyJR18 <manzanares.santiago@hotmail.com>
2023-06-05 20:32:34 -0600

    a punto de realizar un nuevo frm con nuevas funcionalidades

--- ProyectoF/forms/FrmDiagnostico.vb
+++ ProyectoF/forms/FrmDiagnostico.vb
@@ -1,3 +1,12 @@
 ﻿Public Class FrmDiagnostico
 
+#Region "Botones del Formulario"
+    Private Sub BtnCerrar_Click(sender As Object, e As EventArgs) Handles BtnCerrar.Click
+        Me.Close()
+    End Sub
+#End Region
+
+#Region "Funciones del Formulario"
+
+#End Region
 End Class
